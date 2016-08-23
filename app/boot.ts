706b4ae84d1ca203f97801e020ab2b0442691299
James Barnett <James_Barnett@hms.harvard.edu>
2016-08-23 11:09:38 -0400

    angular2 now running in production mode.

--- app/boot.ts
+++ app/boot.ts
@@ -3,5 +3,7 @@ import {SimpleRouting} from './app.component.ts'
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {provide} from 'angular2/core';
 import {LocationStrategy, Location, HashLocationStrategy } from 'angular2/router';
+import {enableProdMode} from 'angular2/core'
+enableProdMode();
 bootstrap(SimpleRouting,[ROUTER_PROVIDERS,
     provide(LocationStrategy, {useClass: HashLocationStrategy})]);
